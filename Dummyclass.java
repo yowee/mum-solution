1e684dbf64479db1279ddf5900fa76ed5b1e755c
yowee <yomefisseha43@gmail.com>
2021-04-09 22:37:50 -0500

    Update Dummyclass.java

--- Dummyclass.java
+++ Dummyclass.java
@@ -1,8 +1,9 @@
+
 public class Dummyclass {
 
 	public static void main(String[] args) {
-		int example2 = isSequencedArray(new int[] {1,3,4,2,5},1,5);
 		int example1 = isSequencedArray(new int[] {1, 2, 3, 4, 5},1,5);
+		int example2 = isSequencedArray(new int[] {1,3,4,2,5},1,5);
 		int example3 = isSequencedArray(new int[] {-5, -5, -4, -4, -4, -3, -3, -2, -2, -2},-5,-2);
 		int example4 = isSequencedArray(new int[] {0, 1, 2, 3, 4, 5},1,5);
 		int example5 = isSequencedArray(new int[] {1, 2, 3, 4},1,5);
@@ -14,15 +15,16 @@ public class Dummyclass {
 		System.out.println("Exmaple 5: "+ example5);
 		System.out.println("Exmaple 6: "+ example6);
 	}
-	
-	static int isSequencedArray(int[ ] a, int m, int n) {
-		if(a[0] != m || a[a.length-1]!= n) return 0;
+
+	int isSequencedArray(int[ ] a, int m, int n) {
+		if(a[0] != m || a[a.length-1] != n) return 0;
 		
-		for (int i = 1; i < a.length-1; i++) {
-			if(a[i+1]-a[i] != 0 && a[i+1]-a[i]!=1) return 0;
+		for (int i = 0; i < a.length-1; i++) {
+			if(a[i+1]-a[i] != 0 && a[i+1]-a[i] != 1) return 0;			
 		}
 		
 		return 1;
 	}
+
 
 }
